1ae4e578c306842c14466583250b98a1ce391dbf
Mike DeGan <mikedegan@gmail.com>
2024-02-02 10:44:48 -0500

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+  return {
+    id: "member",
+    name: "Member",
+    description: "Member of the team",
+    icon: "user",
+    color: "var(--vscode-textLink-foreground)",
+  };
+}
